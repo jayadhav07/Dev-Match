aad4606467f2ceb3b579169a052b9b25651f021f
jayadhav07 <jayadhav786@gmail.com>
2025-10-09 23:14:18 +0530

    updated

--- devMatchBackend/src/app.js
+++ devMatchBackend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://dev-match-anha.vercel.app",
     credentials: true,
   })
 );
